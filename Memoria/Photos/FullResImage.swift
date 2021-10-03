c01d8f4e398cc58f159d7277b874c7029eff7bd0
Sagar Patel <s.72427patel@gmail.com>
2021-10-03 00:19:52 -0400

    Added Full Res Image Class

---
+++ Memoria/Photos/FullResImage.swift
@@ -0,0 +1,20 @@
+//
+//  FullResImage.swift
+//  Memoria
+//
+//  Created by Sagar R Patel on 2021-10-03.
+//
+
+import SwiftUI
+
+struct FullResImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FullResImage_Previews: PreviewProvider {
+    static var previews: some View {
+        FullResImage()
+    }
+}
